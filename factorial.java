1634d66a1aa18cb2e7531c1cc2eefdea9ed45c42
Tejal Powar <tejalpowar53@gmail.com>
2025-03-13 23:54:26 +0530

    Added new file: factorial.java

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+ class factorial 
+ { 
+  public static void main(String args[]) 
+  { 
+   int n, fact=1; 
+   Scanner sc= new Scanner(System.in);   
+   System.out.println(“Enter the number:”); 
+   n=sc.nextInt(); 
+   for(int I=1;i<=1;i++) 
+   { 
+    fact=fact*I; 
+   } 
+   System.out.println(“Factorial= ” +fact); 
+  } 
+ }
